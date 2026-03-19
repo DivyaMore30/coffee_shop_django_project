c4f5b6ad230b137ed58971b24be248d9c83b4da4
divya <divya.more@krawlnet.com>
2026-03-19 19:34:12 +0530

    Demo commit

--- coffeeshop/store/models.py
+++ coffeeshop/store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+#Product Model
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -12,7 +13,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
+#Cart Model
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
